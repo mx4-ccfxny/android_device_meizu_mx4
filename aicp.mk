886293b544ff7db99ee24bec5e0fbbc2d28e48e8
ccfxny <9886712@126.com>
2016-09-13 11:54:54 +0800

    bringup aicp6.0

--- aicp.mk
+++ aicp.mk
@@ -4,14 +4,15 @@
 PRODUCT_RELEASE_NAME := MX4
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aicp/configs/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/meizu/mx4/device_mx4.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mx4
-PRODUCT_NAME := cm_mx4
+PRODUCT_NAME := aicp_mx4
 PRODUCT_BRAND := Meizu
 PRODUCT_MODEL := MX4
 PRODUCT_MANUFACTURER := Meizu
+
